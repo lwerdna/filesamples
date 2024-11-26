ca2cd367ed00610f1ed21d19d27fa176bb82b6da
Andrew Lamoureux <lwerdna@users.noreply.github.com>
2024-11-26 15:43:17 -0500

    Add linux cooked sll2 capture, tool to replace macs

---
+++ replace_mac_addrs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+mac1, mac2, fpath1, fpath2 = sys.argv[1:]
+
+mac1 = bytes.fromhex(mac1.replace(':', ''))
+mac2 = bytes.fromhex(mac2.replace(':', ''))
+
+print('mac1: ' + mac1.hex())
+print('mac2: ' + mac2.hex())
+
+with open(fpath1, 'rb') as fp:
+    data = fp.read()
+
+n = data.count(mac1)
+if n > 0:
+    with open(fpath2, 'wb') as fp:
+        data = data.replace(mac1, mac2)
+
+print(f'replaced {n} macs')
